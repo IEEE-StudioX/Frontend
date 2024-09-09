345f8c7716905c1c61baf078bda2ac9dfbb97d03
EHAB3x <ehabmah6969@gmail.com>
2024-09-10 01:48:35 +0200

    install important libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@styles/global.css'
+import '@styles/global.css';
+import Swal from 'sweetalert2';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1 className="text-3xl font-bold underline">Welcome To Studio-X</h1>
+    <h1 className="text-3xl font-bold underline"
+    onClick={()=>{
+      Swal.fire("SweetAlert2 is working!");
+    }}>Test tailwind and sweetAlert</h1>
   </StrictMode>,
 )
