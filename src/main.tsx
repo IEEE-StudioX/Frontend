09911d7cf3c887d6434f49afb9bab3630a76d9dc
EHAB3x <ehabmah6969@gmail.com>
2024-09-09 20:59:41 +0200

    install tailwind and important plugins

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import '@styles/global.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Welcome To Studio-X</h1>
+    <h1 className="text-3xl font-bold underline">Welcome To Studio-X</h1>
   </StrictMode>,
 )
